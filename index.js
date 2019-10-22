4a8116febe2722377b2538ab5e3c15352914b60e
jeff <jeff@giftbit.com>
2019-10-22 14:05:19 -0700

    Undo actually complicated changes that could possibly be wrong and just shim the legacy handler.

--- index.js
+++ index.js
@@ -1,48 +1,75 @@
 var aws = require("aws-sdk");
 
 /**
- * A Simple Lambda to look up the User ID of a Role
+ * Shim this legacy code to work on Node 10 while doing as little as possible to change it.
  */
-exports.handler = async function(event, context) {
+exports.handler = async function (event, context) {
+    return new Promise(((resolve, reject) => {
+        legacyHandler(
+            event,
+            {
+                ...context,
+                done: resolve
+            }
+        );
+    }));
+};
+
+/**
+ * A Simple Lambda to look up the User ID of a Role
+ **/
+function legacyHandler(event, context) {
 
     console.log("REQUEST RECEIVED:\n" + JSON.stringify(event));
 
     // For Delete requests, immediately send a SUCCESS response.
-    if (event.RequestType === "Delete") {
+    if (event.RequestType == "Delete") {
         sendResponse(event, context, "SUCCESS", event.PhysicalResourceId);
         return;
     }
 
-    const roleNames = (event.ResourceProperties.RoleNames || "")
+    var roleNames = (event.ResourceProperties.RoleNames || "")
         .split(",")
         .concat(event.ResourceProperties.RoleName)
-        .map(r => r ? r.trim() : r)
-        .filter(r => !!r);
+        .map(function (r) { return r ? r.trim() : r; })
+        .filter(function (r) { return r; });
 
-    const iam = new aws.IAM();
-    const resultPromises = roleNames.map(roleName => new Promise((resolve, reject) => {
+    var iam = new aws.IAM();
+    var hasErrored = false;
+    var results = [];
+
+    roleNames.forEach(function (roleName, ix) {
         iam.getRole({
             RoleName: roleName
-        }, function (err, getRoleResult) {
+        }, function(err, getRoleResult) {
+            if (hasErrored) {
+                return;
+            }
+
             if (err) {
-                reject(err);
+                hasErrored = true;
+                var responseData = {Error: "Failed to retrieve the Role with Role Name: " + roleName}
+                console.log(responseData.Error + ":\n", err);
+                sendResponse(event, context, "FAILED", context.logStreamName);
             } else {
-                resolve(getRoleResult.Role.RoleId);
+                results[ix] = getRoleResult.Role.RoleId;
+
+                // Check if we've got all results.
+                for (var i=0; i < roleNames.length; i++) {
+                    if (!results[i]) {
+                        // We don't.
+                        return;
+                    }
+                }
+
+                if (event.ResourceProperties.AsGrant !== false && event.ResourceProperties.AsGrant !== "false") {
+                    results = results.map(function (r) { return r + ":*" });
+                }
+
+                sendResponse(event, context, "SUCCESS", results.join(","));
             }
         });
-    }));
-
-    try {
-        let results = await Promise.all(resultPromises);
-        if (event.ResourceProperties.AsGrant !== false && event.ResourceProperties.AsGrant !== "false") {
-            results = results.map(r => r + ":*");
-        }
-        sendResponse(event, context, "SUCCESS", results.join(","));
-    } catch (err) {
-        var responseData = {Error: "Failed to retrieve the Role with Role Name: " + roleName};
-        console.log(responseData.Error + ":\n", err);
-        sendResponse(event, context, "FAILED", context.logStreamName);
-    }
+    });
 };
 
 // Send response to the pre-signed S3 URL
@@ -60,11 +87,11 @@ function sendResponse(event, context, responseStatus, physicalResourceId) {
 
     console.log("RESPONSE BODY:\n", responseBody);
 
-    const https = require("https");
-    const url = require("url");
+    var https = require("https");
+    var url = require("url");
 
-    const parsedUrl = url.parse(event.ResponseURL);
-    const options = {
+    var parsedUrl = url.parse(event.ResponseURL);
+    var options = {
         hostname: parsedUrl.hostname,
         port: 443,
         path: parsedUrl.path,
@@ -77,22 +104,20 @@ function sendResponse(event, context, responseStatus, physicalResourceId) {
 
     console.log("SENDING RESPONSE...\n");
 
-    return new Promise((resolve, reject) => {
-        const request = https.request(options, function(response) {
-            console.log("STATUS: " + response.statusCode);
-            console.log("HEADERS: " + JSON.stringify(response.headers));
-            // Tell AWS Lambda that the function execution is done
-            resolve();
-        });
-
-        request.on("error", function(error) {
-            console.log("sendResponse Error:" + error);
-            // Tell AWS Lambda that the function execution is done
-            reject(error);
-        });
-
-        // write data to request body
-        request.write(responseBody);
-        request.end();
+    var request = https.request(options, function(response) {
+        console.log("STATUS: " + response.statusCode);
+        console.log("HEADERS: " + JSON.stringify(response.headers));
+        // Tell AWS Lambda that the function execution is done
+        context.done();
     });
+
+    request.on("error", function(error) {
+        console.log("sendResponse Error:" + error);
+        // Tell AWS Lambda that the function execution is done
+        context.done();
+    });
+
+    // write data to request body
+    request.write(responseBody);
+    request.end();
 }
